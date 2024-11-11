e5c1628facdad03f0710439e4dd2b2c38fb66f7f
kumar jyothiranjan <108728582+jyothiranjan25@users.noreply.github.com>
2024-11-11 14:50:37 +0530

    Delete auth directory

--- auth/src/main/java/com/example/auth/exception/UserAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.example.auth.exception;
-
-public class UserAlreadyExistsException extends RuntimeException {
-    public UserAlreadyExistsException(String message) {
-        super(message);
-    }
-}
